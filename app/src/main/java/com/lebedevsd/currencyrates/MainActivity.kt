7acd411773f5ece114a4a7a58a8b494c88d768a6
Sergey Lebedev <Lebedevsd@gmail.com>
2019-11-03 21:46:36 +0100

    Implemented styles

--- app/src/main/java/com/lebedevsd/currencyrates/MainActivity.kt
+++ app/src/main/java/com/lebedevsd/currencyrates/MainActivity.kt
@@ -3,9 +3,7 @@ package com.lebedevsd.currencyrates
 import android.os.Bundle
 import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
 import dagger.android.support.DaggerAppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : DaggerAppCompatActivity() {
 
@@ -15,16 +13,12 @@ class MainActivity : DaggerAppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
         val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-        NavigationUI.setupActionBarWithNavController(this, navController, null)
         appBarConfiguration = AppBarConfiguration.Builder(navController.graph).build()
-
-        NavigationUI.setupWithNavController(toolbar, navController)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-    }
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+//        return NavigationUI.navigateUp(navController, appBarConfiguration)
+//    }
 }
